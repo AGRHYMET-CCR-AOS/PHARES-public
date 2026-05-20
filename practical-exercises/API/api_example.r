2e9668809aa2776eabdf9f2a57249e7c52dc3c31
Jafet Andersson <jcmand@users.noreply.github.com>
2026-05-20 13:38:17 +0000

    Create api_example.r

---
+++ practical-exercises/API/api_example.r
@@ -0,0 +1,37 @@
+############################################################
+# R script for accessing a restricted PHARES APIs with a key
+############################################################
+
+####################
+#  Settings
+
+# Define your working directory, where downloaded files will be stored
+  home_dir <- "C:/Temp"       
+
+# Define the path and name of the API key file (JSON)
+  key_path_json <- file.path(Sys.getenv("HOME"),"phares",".phares-was-fastapi-key",fsep = "\\")
+
+# Define the path and name of the script containing the code for executing the API request with a JSON Key file
+  api_request_script_name <- "C:/Temp/api_request_with_key.R"    
+
+# Define target_url, i.e. the API endpoint that you want to use
+  # List of available APIs: https://github.com/AGRHYMET-CCR-AOS/PHARES-public/wiki/APIs-for-data-access
+  # Each API then lists the endpoints available on the API main page. Examples for https://api-was.phares-agrhymet.cilss.int/
+    # https://api-was.phares-agrhymet.cilss.int/api/storage/list
+    # https://api-was.phares-agrhymet.cilss.int/api/storage/latest/list/{zone_name}
+    # https://api-was.phares-agrhymet.cilss.int/api/storage/latest/download/{zone_name}
+
+ target_url <- "https://api-was.phares-agrhymet.cilss.int/api/storage/list"
+# target_url <- "https://api-was.phares-agrhymet.cilss.int/api/storage/latest/list/wa-hype1.2_hydrogfd3.2_ecoper_noINSITU"
+# target_url <- "https://api-was.phares-agrhymet.cilss.int/api/storage/latest/download/wa-hype1.2_hydrogfd3.2_ecoper_noINSITU"
+
+# Define client ID (no need to change normally)
+client_id  <- "89447740177-oa9r2j411ugm9cu6k15qjsi3jkiao2kp.apps.googleusercontent.com" 
+
+#######################
+# Run the API request
+
+system2(
+  command = "Rscript",
+  args = c(api_request_script_name, target_url, client_id, home_dir, key_path_json)
+)
