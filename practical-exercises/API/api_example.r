af02ce1777c7721062a7cee91a49b20fd381fc6a
kiema97 <statplus.dec@gmail.com>
2026-05-22 00:57:49 +0000

    removing hydro_bulletin folder

--- practical-exercises/API/api_example.r
+++ practical-exercises/API/api_example.r
@@ -4,15 +4,15 @@
 
 ####################
 #  Settings
-
+base_path <- "D:/CCR_AOS/PROJETS/TransfertFANFAR/Activites2026/Training3/repositories/PHARES-public/practical-exercises/API"
 # Define your working directory, where downloaded files will be stored
-  home_dir <- "C:/Temp"       
+  home_dir <- "D:/CCR_AOS/ACTIVITES/AGRHYMET/2026/DCEM/Dev/bulletin"      
 
 # Define the path and name of the API key file (JSON)
-  key_path_json <- file.path(Sys.getenv("HOME"),"phares",".phares-was-fastapi-key",fsep = "\\")
+  key_path_json <- file.path(Sys.getenv("HOME"),"phares","phares-fastapi-was-key",fsep = "\\")
 
 # Define the path and name of the script containing the code for executing the API request with a JSON Key file
-  api_request_script_name <- "C:/Temp/api_request_with_key.R"    
+  api_request_script_name <- file.path(base_path,"api_request_with_key.R")  
 
 # Define target_url, i.e. the API endpoint that you want to use
   # List of available APIs: https://github.com/AGRHYMET-CCR-AOS/PHARES-public/wiki/APIs-for-data-access
@@ -21,9 +21,9 @@
     # https://api-was.phares-agrhymet.cilss.int/api/storage/latest/list/{zone_name}
     # https://api-was.phares-agrhymet.cilss.int/api/storage/latest/download/{zone_name}
 
- target_url <- "https://api-was.phares-agrhymet.cilss.int/api/storage/list"
-# target_url <- "https://api-was.phares-agrhymet.cilss.int/api/storage/latest/list/wa-hype1.2_hydrogfd3.2_ecoper_noINSITU"
-# target_url <- "https://api-was.phares-agrhymet.cilss.int/api/storage/latest/download/wa-hype1.2_hydrogfd3.2_ecoper_noINSITU"
+#target_url <- "https://api-was.phares-agrhymet.cilss.int/api/storage/list"
+target_url <- "https://api-was.phares-agrhymet.cilss.int/api/storage/latest/list/wa-hype1.2_hydrogfd3.2_ecoper_noINSITU"
+target_url <- "https://api-was.phares-agrhymet.cilss.int/api/storage/latest/download/wa-hype1.2_hydrogfd3.2_ecoper_noINSITU"
 
 # Define client ID (no need to change normally)
 client_id  <- "89447740177-oa9r2j411ugm9cu6k15qjsi3jkiao2kp.apps.googleusercontent.com" 
